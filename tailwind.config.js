d385f0aada9aaa4cf20e98a19f1904137222db48
Emanuel Cruz <emanuelcruz091@gmail.com>
2024-03-20 00:50:56 -0300

    config: custom fonts colores size

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./**/*.html",
-  ],
+  content: ["./**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        tomato: "hsl(4, 100%, 67%)",
+        "dark-slate-grey": "hsl(234, 29%, 20%)",
+        "charcoal-Grey": "hsl(235, 18%, 26%)",
+        "custom-grey": "hsl(231, 7%, 60%)",
+        "custom-white": "hsl(0, 0%, 100%)",
+      },
+      fontSize: {
+        paragraph: "16px",
+      },
+      fontFamily: {
+        primary: "roboto",
+      },
+    },
   },
   plugins: ["prettier-plugin-tailwindcss"],
-}
-
+};
